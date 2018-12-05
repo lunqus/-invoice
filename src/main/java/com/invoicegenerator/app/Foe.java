167d8a8ad5ae36545d53e48ebbf148d8d411c94c
LUNKUS <sigitas@lunkus.com>
2018-12-05 11:30:46 +0200

    foe addition

---
+++ src/main/java/com/invoicegenerator/app/Foe.java
@@ -0,0 +1,34 @@
+package com.invoicegenerator.app;
+
+public class Foe {
+
+    public void seller() {
+
+        String seller;
+        String sellerCode;
+        String sellerVat;
+        String sellerAddress;
+        String sellerAddressCity;
+        String sellerAddressCountry;
+        String sellerBank;
+
+        seller = "UAB \"ABC\"";
+        System.out.println( "Pardavejas: " + seller);
+    }
+
+    public void customer() {
+
+        String customer;
+        String customerCode;
+        String customerVat;
+        String customerAddress;
+        String customerAddressCity;
+        String customerAddressCountry;
+        String customerBank;
+
+        customer = "UAB \"XYZ\"";
+        System.out.println( "Pirkejas: " + customer);
+    }
+
+}
+
